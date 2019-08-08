42d5dc3e4a8011ceeaea67cb572642b1b62a633c
Andre Holguin <andreesito@gmail.com>
2019-08-07 17:05:22 -0700

    two spinner components created - fixed img source bug for zeroHedge relative paths

--- src/components/UI/Card.vue
+++ src/components/UI/Card.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="cardContainer">
-        <img :src="imgSrc" :alt="imgSrc">
+        <img :src="relativePathFilter(imgSrc)" :alt="imgSrc">
+        <span>{{domain}}.com</span>
         <div>
         <h2>{{titleSrc}}</h2>
-        <h3><i>{{authorSrc}}</i></h3>
-        <p>{{dateSrc}}</p>
-        <p>{{summarySrc}}</p>
-        <a :href="linkSrc">Source</a>
+        <h3 v-if="authSrc"><i>{{authorSrc}}</i></h3>
+        <p v-if="dateSrc">{{dateSrc}}</p>
+        <p v-if="summarySrc">{{summarySrc}}</p>
+        <a :href="linkSrc" target="_blank">Source</a>
         </div>
         
     </div>
@@ -15,12 +16,21 @@
 <script>
     export default {
         props: [
+            'domain',
             'imgSrc', 
             'authorSrc',
             'titleSrc', 
             'dateSrc', 
             'summarySrc', 
             'linkSrc'],
+        methods: {
+            relativePathFilter(strg) {
+                return strg.match(/\b(\w*https\w*)\b/g) ?
+                            this.imgSrc
+                            :
+                            `https://www.${this.domain}.com/${this.imgSrc.slice(1)}`;
+            }
+        }
         };
         
         
@@ -30,21 +40,26 @@
 <style lang="scss" scoped>
     .cardContainer {
         position: relative;
-        width: 20vw;
-        
+        width: 24vw;
+        align-self: auto;
         margin: 1rem;
         flex:1 0 auto;
         background-color: rgba(white, 0.8);
         
+        
         img {
             width: 100%;
             height:40%;
+            margin-bottom: 1rem;
+        }
+        span {
+            font-size: .9rem;
         }
         div {
             padding: .5rem 1rem 1rem 1rem;
             h2 {
                 position: absolute;
-                top:3rem;
+                top:1rem;
                 left:0rem;
                 padding: 1rem;
                 background-color: rgba(white,0.8);
@@ -64,6 +79,7 @@
                 color: #62716f;
                 padding:.5rem 2rem;
                 border-radius: .4rem;
+                
             }
         }
     }
