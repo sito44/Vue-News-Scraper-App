9d6241900615b416c2f32858849b9d94c956ce16
Andre Holguin <andreesito@gmail.com>
2019-08-28 18:35:19 -0700

    responsive design

--- src/components/UI/CircleButton.vue
+++ src/components/UI/CircleButton.vue
@@ -5,12 +5,13 @@
 
 
 <style lang="scss" scoped>
+@import '../../Sass/abstracts/_mixins.scss';
     button {
         position: fixed;
         bottom: 2rem;
         right:2rem;
-        width: 5vw;
-        height: 12vh;
+        width: 5%;
+        height: 10%;
         border-radius: 100%;
         border: none;
         transform: rotate(-90deg);
@@ -18,7 +19,33 @@
         background-repeat: no-repeat;
         background-position: center;
         background-size: 85%;
-        box-shadow: -2px 1px 5px black;
+        box-shadow: -2px 0px 4px black;
+        opacity: .9;
+        @include respond(big-desktop){
+            height:10vh;
+            width: 5vw;
+            
+        }
+        @include respond(desktop) {
+            height:10vh;
+            width: 5vw;
+            
+         }
+        @include respond(tab-land) {
+            height:10vh;
+            width: 7vw;
+        }
+        @include respond(tab-port) {
+            height:6vh;
+            width: 8vw;
+        }
+        @include respond(phone) {
+            height:10vh;
+            width: 18vw;
+            bottom: 1rem;
+            right:1rem;
+            
+        }
         
     }
 </style>
